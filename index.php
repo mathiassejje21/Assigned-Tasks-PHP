0d511656d38aa99ee3d32357b24a380ea4c4054f
mathiassejje21 <mathiassejje@gmail.com>
2024-10-08 21:00:35 -0500

    first interfaz

--- index.php
+++ index.php
@@ -3,11 +3,19 @@ include("./Layout/header.php")
 ?>
 
 <div class="containerPrincipal">
-    <header>Assigned Tasks | Programming</header>
-    <section class="cotainerSecundario">
-
+    <header> | Programming | </header>
+    <section class="cotainerAssigned">
+        <p>Assigned Tasks</p>
+        <?php
+        require_once("./Controllers/conexion.php");
+        require_once("./Controllers/insert.php") ?>
+        <form action="index.php" method="post">
+            <input name="nombre" class="nametarea" maxlength="30" type="text" placeholder="Nombre de la tarea" required>
+            <textarea name="descripcion" rows="4" placeholder="Descripcion" required></textarea>
+            <input name="crear" id="button" type="submit" value="Crear">
+        </form>
     </section>
-    <section class="cotainerSecundario">
+    <section class="containertasks">
 
     </section>
 
