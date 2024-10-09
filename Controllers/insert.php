ecf1e1def35a8fd0eff1d62f86e2275585201f58
mathiassejje21 <mathiassejje@gmail.com>
2024-10-09 10:42:26 -0500

    verificar

--- Controllers/insert.php
+++ Controllers/insert.php
@@ -1,17 +1,27 @@
 <?php
 
 if (!empty($_POST["crear"])) {
+    $vername = htmlspecialchars(trim($_POST["nombre"]));
+    $verdescription = htmlspecialchars(trim($_POST["descripcion"]));
 
-    $nombre = $_POST['nombre'];
-    $text = $_POST['descripcion'];
 
-    // Preparar la consulta
-    $sql = "INSERT INTO tareas(nombre, texttareas) VALUES(?, ?)";
-    $stms = $conexion->prepare($sql);
+    if (!empty($vername) && !empty($verdescription)) {
+        $nombre = $_POST['nombre'];
+        $text = $_POST['descripcion'];
 
-    $stms->bind_param("ss", $nombre, $text);
-    $stms->execute();
+        // Preparar la consulta
+        $sql = "INSERT INTO tareas(nombre, texttareas) VALUES(?, ?)";
+        $stms = $conexion->prepare($sql);
 
-    echo '<div>Creado Correctamente</div>';
-    $stms->close();
+        $stms->bind_param("ss", $nombre, $text);
+        $stms->execute();
+?>
+        <div class="message-correct">Tarea creada correctamente.</div>
+    <?php
+        $stms->close();
+    } else {
+    ?>
+        <div class="message-error">Rellene todo los campos.</div>
+<?php
+    }
 }
